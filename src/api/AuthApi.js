6c1c8856f96735ac7f310647b921a81c612b27c7
Chuli jimmi Manurung <jimmi.ext@gmail.com>
2020-03-03 09:32:37 +0700

    fixed payload request

--- src/api/AuthApi.js
+++ src/api/AuthApi.js
@@ -5,7 +5,7 @@ import HttpFetch from './HttpFetch'
  * reducers Registration
  * @param {Object} payload 
  */
-async function register (payload) {
+async function register ({payload}) {
     const url = 'https://mitraist.herokuapp.com/auth/signup'
     const resp = await HttpFetch.appPost(url, payload)
     return resp;
